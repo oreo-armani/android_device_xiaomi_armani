1b71612355ff6fe6b79bc9c2e921bf5c67345171
haran <harangovindan@gmail.com>
2018-03-17 16:52:32 +0200

    armani: Update Camera Dependencies to O
    * Move camera-wrapper to vendor
    * Add proper dependencies

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,18 +1,38 @@
+
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
-
-LOCAL_C_INCLUDES += \
+LOCAL_C_INCLUDES := \
+    framework/native/include \
     system/media/camera/include
 
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_SHARED_LIBRARIES := \
+    libhardware \
+    libbase \
+    libhidltransport \
+    liblog \
+    libcamera_client \
+    libutils \
+    libcutils \
+    libgui \
+    libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
+
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect \
+    libbase
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := camera.msm8226
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
