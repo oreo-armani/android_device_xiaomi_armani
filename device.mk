30723e581330772426d18f5000e2c096e9ed276e
haran <harangovindan@gmail.com>
2018-03-18 10:35:09 +0200

    armani: Add camera props and flags
    * This forces HAL1 on camera apps
    * Adds video recording props needed for legacy HAL1
    * Remove unwanted and useless camera props and flags
    * Move props from device.mk to system.prop

--- device.mk
+++ device.mk
@@ -152,11 +152,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true \
-    persist.media.treble_omx=false
-
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
